975dcec6c90ccd4e7711c05a96b3f0c4c32ba156
Hi <fbjz5k8t5h@privaterelay.appleid.com>
2026-01-21 21:36:41 -0600

    Refactor 420Hub loader to remove token usage
    Simplified the loading process by removing the token and headers.

--- 420Hub_PROTECTED.lua
+++ 420Hub_PROTECTED.lua
@@ -1,12 +1,3 @@
--- 420 HUB public loader
--- real code is NOT here
-
-local TOKEN = "PUT_GITHUB_TOKEN_HERE"
+-- 420 HUB loader (public, safe)
 local URL = "https://raw.githubusercontent.com/fbjz5k8t5h-blip/420Hub-Core/main/main.lua"
-
-local headers = {
-	["Authorization"] = "token "..TOKEN
-}
-
-local source = game:HttpGet(URL, true, headers)
-loadstring(source)()
+loadstring(game:HttpGet(URL))()
